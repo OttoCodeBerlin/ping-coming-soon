9a32632ee4e6fab0f62cfabb2f6171c242886249
Otto Schmid <otto.schmid@Ottos-MacBook-Pro.local>
2020-04-24 23:50:46 +0200

    Added things.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './images/logo.svg'
+import illustration from './images/illustration-dashboard.png'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} alt="" />
+      <h1>We are launching soon!</h1>
+      <h5>Subscribe and get notified</h5>
+      <form action="" method="post">
+        <input type="email" name="" id="" placeholder="Your email address..." />
+        <input type="button" value="Notify Me" />
+      </form>
+      <img className="illustration" src={illustration} alt="" />
+      <a href="www.facebook.com" class="fa fa-facebook blue">
+        {' '}
+      </a>
+      <a href="www.twitter.com" className="fa fa-twitter blue">
+        {' '}
+      </a>
+      <a href="www.instagram.com" className="fa fa-instagram blue">
+        {' '}
+      </a>
+      <br/>
+      <small>&copy; Copyright Ping. All rights reserved.</small>
+      <footer>
+        <small>
+          Challenge by{' '}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://github.com/OttoCodeBerlin">OttoCodeBerlin</a>.
+        </small>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
